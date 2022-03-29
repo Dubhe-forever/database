c179e5a4c8d315e1192734295959a5ed3dc76df5
Dubhe-forever <95358118+Dubhe-forever@users.noreply.github.com>
2022-03-29 20:24:02 +0800

    Update db.sql
    数据库的四张表的sql代码：用户、院校历史录取数据表、院校排名表、省高考分数排名表

--- db.sql
+++ db.sql
@@ -1,5 +1,27 @@
-create table stu(
-    id varchar(10) primary key,
-    grade int,
-    srange int
+create table GradeRange(            #江苏省高考分数排名表
+    grade int primary key,          #成绩
+    srange int                      #排名
+);
+create table User(                  #用户表
+    id varchar(10) primary key,     #用户id
+    grade int,                      #用户成绩  
+    passwords varchar(10),          #密码
+    Major varchar(10),              #意向专业
+    notes varchar(50)               #备注
+);
+create table SchoolRange(           #院校排名表
+    SchoolNum int primary key,      #院校编号
+    SchoolName varchar(10),         #名称
+    SchoolRange int,                #排名
+    Major varchar(10)               #专业
+);
+create table HistorySchoolData(     #院校历史录取数据表
+    id varchar(10),                 #历史用户id
+    srange int,                     #用户排名
+    Syear int,                      #年份
+    SchoolNum int,                  #院校编码
+    SchoolName varchar(10),         #院校名称
+    primary key(id,SchoolNum),
+    foreign key(id) references User(id),
+    foreign key(SchoolNum) references SchoolRange(SchoolNum)
 );
