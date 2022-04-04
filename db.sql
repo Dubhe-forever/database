677124341224f154e4c74f5b6eb7aad5121c81ab
Dubhe-forever <95358118+Dubhe-forever@users.noreply.github.com>
2022-04-04 21:51:55 +0800

    Update db.sql
    update

--- db.sql
+++ db.sql
@@ -7,13 +7,13 @@ create table User(                  #用户表
     id varchar(10) primary key,     #用户id
     grade int,                      #用户成绩  
     passwords varchar(10),          #密码
-    Major varchar(10),              #意向专业
+    Major varchar(20),              #意向专业
     notes varchar(50)               #备注
 );
 
 create table School(
     SchoolNum varchar(10) primary key,          #院校编号
-    SchoolName varchar(10)                     #名称
+    SchoolName varchar(20)                     #名称
 );
 
 create table SchoolRangeCS(         #计算机院校排名表
@@ -34,8 +34,8 @@ create table HistorySchoolDataCS(   #计算机院校历史录取数据表
     HSrange int,                    #用户最高排名
     LSrange int,                    #用户最低排名
     Syear int,                      #年份  建立索引index
-    SchoolNum int,                  #院校编码
-    SchoolName varchar(10),         #院校名称
+    SchoolNum varchar(10),                  #院校编码
+    SchoolName varchar(20),         #院校名称
     primary key(SchoolNum,Syear),
     foreign key(SchoolNum) references SchoolRangeCS(SchoolNum)
 );
@@ -45,8 +45,8 @@ create table HistorySchoolDataES(   #电信院校历史录取数据表
     HSrange int,                    #用户最高排名
     LSrange int,                    #用户最低排名
     Syear int,                      #年份
-    SchoolNum int,                  #院校编码
-    SchoolName varchar(10),         #院校名称
+    SchoolNum varchar(10),                  #院校编码
+    SchoolName varchar(20),         #院校名称
     primary key(SchoolNum,Syear),
     foreign key(SchoolNum) references SchoolRangeES(SchoolNum)
 );
